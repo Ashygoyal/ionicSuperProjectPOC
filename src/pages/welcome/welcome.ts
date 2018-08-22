7343866e689f70fd7f5ac349776f259563fc5aab
Ashish Goyal <goyal.ashish3@gmail.com>
2018-08-22 16:30:57 +0530

    Displayed base64 value in alert

--- src/pages/welcome/welcome.ts
+++ src/pages/welcome/welcome.ts
@@ -35,7 +35,7 @@ export class WelcomePage {
       // imageData is either a base64 encoded string or a file URI
       // If it's base64 (DATA_URL):
       let base64Image = 'data:image/jpeg;base64,' + imageData;
-      console.log(base64Image);
+      alert(base64Image);
      }, (err) => {
       // Handle error
      });
